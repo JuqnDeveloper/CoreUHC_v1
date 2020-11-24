95f81e08e7707cf62d6fa4838b5cae296796242a
JuqnDeveloper <74925367+JuqnDeveloper@users.noreply.github.com>
2020-11-23 23:24:32 -0300

    Add files via upload

--- src/Compact/AutoTweet/APITwitter.php
+++ src/Compact/AutoTweet/APITwitter.php
@@ -1,1 +1,52 @@
+<?php
 
+namespace Compact\AutoTweet;
+
+use Compact\Loader;
+use pocketmine\utils\Config;
+
+class APITwitter {
+	
+	private $plugin;
+	
+	public function __construct(Loader $plugin){
+		$this->plugin = $plugin;
+	}
+	
+	public function getScenarios(){
+		$scenarios = array();
+		foreach($this->plugin->configuration['Scenarios'] as $scenario => $active){
+			if($this->plugin->configuration['Scenarios'][$scenario] == true){
+				array_push($scenarios, $scenario);
+			}
+		}
+		$text = implode(", ", $scenarios);
+		return $text;
+	}
+
+	public function getHosts(){
+	    $hosters = array();
+	    foreach ($this->plugin->getArena()->getPlayers() as $players){
+	        if($players->isHost()){
+	            array_push($hosters, $players->getName());
+            }
+        }
+	    $text = implode(" / ", $hosters);
+	    return $text;
+    }
+	
+	public function publicTweetStartUHC($type, $typeMode, $whitelist){
+        $config = new Config($this->plugin->getDataFolder()."Config.yml", Config::YAML);
+        $config->set("HostInt", $config->get("HostInt") + 1);
+        $config->save();
+		$this->plugin->publicTweetStartUHC($type, $typeMode, $whitelist, $this->getScenarios(), $this->getHosts());
+	}
+
+	public function publicWinner($player, $kills){
+	    $this->plugin->publicWinner($player, $kills);
+    }
+
+	public function publicWinnerTeam($players){
+	    $this->plugin->publicWinnerTeam($players);
+    }
+}
